59f56b2570bb301f614c29eee0cddab8308d5112
Alexandre <alejunior.dev@gmail.com>
2024-01-10 14:20:00 -0300

    feat: slices

---
+++ 6/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	slice := []int{10, 20, 30, 40, 50, 60, 70, 80, 90, 100}
+	fmt.Printf("len=%d cap=%d %v\n", len(slice), cap(slice), slice)
+
+	fmt.Printf("len=%d cap=%d %v\n", len(slice[:0]), cap(slice[:0]), slice[:0])
+
+	fmt.Printf("len=%d cap=%d %v\n", len(slice[:4]), cap(slice[:4]), slice[:4])
+
+	fmt.Printf("len=%d cap=%d %v\n", len(slice[2:]), cap(slice[2:]), slice[2:])
+
+	slice = append(slice, 110)
+	fmt.Printf("len=%d cap=%d %v\n", len(slice), cap(slice), slice)
+}
