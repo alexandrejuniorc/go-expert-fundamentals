99c79f8803595f926844f141e33f5dfd0a38b833
Alexandre Junior <alejunior.dev@gmail.com>
2024-01-18 21:08:50 -0300

    feat: conditionals

---
+++ 20/main.go
@@ -0,0 +1,22 @@
+package main
+
+func main() {
+	a := 10
+	// b := 20
+	// c := 30
+
+	switch a {
+	case 1:
+		println("a")
+	case 2:
+		println("b")
+	case 3:
+		println("c")
+	default:
+		println("d")
+	}
+
+	// if a > b || c > a {
+	// 	println("a > b && c > a")
+	// }
+}
