5132f13a74006e38d1980f571f80d347ad810309
Alexandre Junior <alejunior.dev@gmail.com>
2024-01-22 22:21:47 -0300

    feat: starting with http

--- major-packages/5-busca-cep/main.go
+++ major-packages/5-busca-cep/main.go
@@ -1,1 +1,53 @@
-package __busca_cep
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+type ViaCEP struct {
+	Cep         string `json:"cep"`
+	Logradouro  string `json:"logradouro"`
+	Complemento string `json:"complemento"`
+	Bairro      string `json:"bairro"`
+	Localidade  string `json:"localidade"`
+	Uf          string `json:"uf"`
+	Ibge        string `json:"ibge"`
+	Gia         string `json:"gia"`
+	Ddd         string `json:"ddd"`
+	Siafi       string `json:"siafi"`
+}
+
+func main() {
+	for _, cep := range os.Args[1:] {
+		request, err := http.Get("https://viacep.com.br/ws/" + cep + "/json/")
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Erro ao fazer requisição: %v\n", err)
+		}
+		defer request.Body.Close()
+
+		response, err := io.ReadAll(request.Body)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Erro ao ler resposta: %v\n", err)
+		}
+
+		var data ViaCEP
+		err = json.Unmarshal(response, &data) // convertendo json para struct
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Erro ao converter resposta: %v\n", err)
+		}
+
+		file, err := os.Create("cidade.txt")
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Erro ao criar arquivo: %v\n", err)
+		}
+		defer file.Close()
+		_, err = file.WriteString(fmt.Sprintf("CEP: %s\nLocalidade: %s\nUF: %s\n", data.Cep, data.Localidade, data.Uf))
+		fmt.Println("Arquivo criado com sucesso!")
+		fmt.Println("Cidade: ", data.Localidade)
+	}
+
+}
