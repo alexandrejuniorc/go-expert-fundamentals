f4667eb4347f7f2c1e84ed9607da037407e539b7
Alexandre Junior <alejunior.dev@gmail.com>
2024-01-24 10:29:44 -0300

    feat: creating a function to search cep

--- busca-cep/main.go
+++ busca-cep/main.go
@@ -1,6 +1,23 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+type ViaCEP struct {
+	Cep         string `json:"cep"`
+	Logradouro  string `json:"logradouro"`
+	Complemento string `json:"complemento"`
+	Bairro      string `json:"bairro"`
+	Localidade  string `json:"localidade"`
+	Uf          string `json:"uf"`
+	Ibge        string `json:"ibge"`
+	Gia         string `json:"gia"`
+	Ddd         string `json:"ddd"`
+	Siafi       string `json:"siafi"`
+}
 
 func main() {
 	http.HandleFunc("/", BuscaCepHandler)
@@ -22,4 +39,25 @@ func BuscaCepHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("Hello World"))
+}
+
+func BuscaCep(cep string) (*ViaCep, error) {
+	response, error := http.Get("https://viacep.com.br/ws/" + cep + "/json/")
+	if error != nil {
+		return nil, error
+	}
+	defer response.Body.Close()
+
+	body, error := io.ReadAll(response.Body)
+	if error != nil {
+		return nil, error
+	}
+
+	var c ViaCep
+	error = json.Unmarshal(body, &c)
+	if error != nil {
+		return nil, error
+	}
+
+	return &c, nil
 }
