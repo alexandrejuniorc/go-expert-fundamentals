9f65ba7143a0f606bcb9c40010c088e8fe98d051
Alexandre Junior <alejunior.dev@gmail.com>
2024-01-25 21:28:20 -0300

    feat: starting with templates

---
+++ major-packages/6-templates/1/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"os"
+	"text/template"
+)
+
+type Curso struct {
+	Nome         string
+	CargaHoraria int
+}
+
+func main() {
+	curso := Curso{Nome: "Curso Go", CargaHoraria: 40}
+	tmp := template.New("CursoTemplate")
+	tmp, _ = tmp.Parse("Curso: {{.Nome}} - Carga Horária: {{.CargaHoraria}}")
+	err := tmp.Execute(os.Stdout, curso)
+	if err != nil {
+		panic(err)
+	}
+}
