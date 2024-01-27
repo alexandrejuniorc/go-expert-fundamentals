c9845eaf50b2f5d42823947996bb33e9695d3fdb
Alexandre Junior <alejunior.dev@gmail.com>
2024-01-26 21:05:45 -0300

    feat: templates with webserver

---
+++ major-packages/6-templates/4/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"net/http"
+	"text/template"
+)
+
+type Curso struct {
+	Nome         string
+	CargaHoraria int
+}
+
+type Cursos []Curso
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		t := template.Must(template.New("template.html").ParseFiles("template.html"))
+		err := t.Execute(w, Cursos{
+			{Nome: "Curso de Go", CargaHoraria: 40},
+			{Nome: "Curso de Python", CargaHoraria: 20},
+			{Nome: "Curso de Java", CargaHoraria: 30},
+		})
+		if err != nil {
+			panic(err)
+		}
+	})
+
+	http.ListenAndServe(":8282", nil)
+}
