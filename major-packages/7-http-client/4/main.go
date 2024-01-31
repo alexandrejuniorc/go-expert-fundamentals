df9f2aab5964f70733aa24035d2b684aff4ba3b1
Alexandre Junior <alejunior.dev@gmail.com>
2024-01-30 22:07:21 -0300

    feat: working with http using contexts

---
+++ major-packages/7-http-client/4/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+	"io"
+	"net/http"
+	"time"
+)
+
+func main() {
+	//ctx, cancel := context.WithCancel(ctx) // cria um contexto que pode cancelar a requisição
+	//ctx, cancel := context.WithDeadline(ctx, time.Now().Add(time.Second)) // o contexto cancela a requisição após 1 segundo
+
+	ctx := context.Background()                               // cria um contexto vazio
+	ctx, cancel := context.WithTimeout(ctx, time.Microsecond) // o contexto cancela a requisição após 1 segundo
+	defer cancel()                                            // cancela a requisição
+
+	request, err := http.NewRequestWithContext(ctx, "GET", "http://google.com", nil) // cria uma requisição com o contexto
+	if err != nil {
+		panic(err)
+	}
+	response, err := http.DefaultClient.Do(request) // faz a requisição
+	if err != nil {
+		panic(err)
+	}
+	defer response.Body.Close() // fecha o corpo da resposta
+
+	body, err := io.ReadAll(response.Body) // lê o corpo da resposta
+	if err != nil {
+		panic(err)
+	}
+	println(string(body)) // imprime o corpo da resposta
+}
