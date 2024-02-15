c344de03acd628673e2ed434d46d0a3199f961b3
Alexandre Junior <alejunior.dev@gmail.com>
2024-02-14 23:01:06 -0300

    feat(database): insert data on database

--- banco-de-dados/1/go.mod
+++ banco-de-dados/1/go.mod
@@ -2,4 +2,7 @@ module github.com/alexandrejuniorc/go-expert-fundamentals
 
 go 1.21.6
 
-require github.com/google/uuid v1.6.0
+require (
+	github.com/go-sql-driver/mysql v1.7.1
+	github.com/google/uuid v1.6.0
+)
--- banco-de-dados/1/main.go
+++ banco-de-dados/1/main.go
@@ -1,6 +1,11 @@
 package main
 
-import "github.com/google/uuid"
+import (
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/google/uuid"
+)
 
 type Product struct {
 	ID    string
@@ -16,6 +21,35 @@ func NewProduct(name string, price float64) *Product {
 		name:  name,
 		price: price,
 	}
+
 }
 
-func main() {}
+func main() {
+	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/goexpert")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	product := NewProduct("Notebook", 1899.90)
+	err = insertProduct(db, product)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func insertProduct(db *sql.DB, product *Product) error {
+	statement, err := db.Prepare("INSERT INTO products (id, name, price) VALUES (?, ?, ?)") // ? é um placeholder para evitar SQL Injection
+	if err != nil {
+		return err
+	}
+
+	defer statement.Close() // é importante fechar o statement após a execução
+
+	_, err = statement.Exec(product.ID, product.name, product.price) // executa a query com os valores passados
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
